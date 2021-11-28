e5407b57b9d097d125875eb049149579e94cba54
patricia andrade <patricia.andrade094@gmail.com>
2021-11-28 02:33:50 -0300

    construcao controllers

--- src/app.js
+++ src/app.js
@@ -1,2 +1,20 @@
 const express = require('express')
 const app = express()
+const port = 3000
+    //Importação do controller
+const funcionario = require('./controller/funcionarios-controller')
+    //Importação do SQLite
+const db = require('./infra-db/sqlite-db')
+
+//----------------------------------------------------------------------------------//
+
+//Middlewares
+app.use(express.json())
+
+//Rotas Funcionarios
+funcionario(app, db)
+
+// Verificação do funcionamento do servidor na porta declarada
+app.listen(port, () => {
+    console.log(`Servidor rodando: http://localhost:${port}/`)
+})
--- src/controller/funcionarios-controller.js
+++ src/controller/funcionarios-controller.js
@@ -2,9 +2,9 @@ const Funcionario = require('../model/funcionarios-model')
 const FuncionarioDAO = require('../DAO/funcionariosDAO')
 
 const funcionario = (app, db) => {
-    const contratadoDAO = new FuncionarioDAO
+    const contratadoDAO = new FuncionarioDAO(db)
 
-    //CREATE
+    //CREATE FUNCIONARIO
     app.post('/funcionario', async(req, res) => {
         try {
             const contratado = new Funcionario(req.body.nomeCompleto, req.body.email, req.body.telefone, req.body.endereco, req.body.RG, req.body.CPF, req.body.dataDeNascimento, req.body.cargo, req.body.turno, req.body.setor, req.body.remuneracao)
@@ -12,11 +12,66 @@ const funcionario = (app, db) => {
             res.json(insereFunc)
 
         } catch (error) {
-            throw new Error(error.message)
+            res.status(400).json({
+                "message": error.message,
+                "error": true
+            })
         }
     })
 
+    // READ - BUSCAR FUNCIONARIO
+    app.get('./funcionario:id', async(req, res) => {
+        const id = parseInt(req.params.id);
+        try {
+            const funcID = await contratadoDAO.buscaFuncionario(id)
+            res.json(funcID)
+        } catch (error) {
+            res.status(400).json({
+                "message": error.message,
+                "error": true
+            })
+        }
+    })
 
+    // READ - LISTAR TODOS OS EMPREGADOS 
+    app.get('./funcionarios', (req, res) => {
+        try {
+            FuncionarioDAO.buscaFuncionarioAll(res)
+        } catch (error) {
+            res.status(400).json({
+                "message": error.message,
+                "error": true
+            })
+        }
+    })
+
+    // UPDATE - ATUALIZA FUNCIONARIO
+    app.patch('./funcionario:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        const valores = req.body
+        try {
+            FuncionarioDAO.atualizaFuncionario(id, valores, res)
+        } catch (error) {
+            res.status(400).json({
+                "message": error.message,
+                "error": true
+            })
+        }
+    })
+
+    // DELETE - FUNCIONARIO DEMITIDO
+    app.delete('/funcionario:id'), async(req, res) => {
+        const id = parseInt(req.params.id)
+        try {
+            const demitido = await contratadoDAO.demiteFuncionario(id)
+            res.json(demitido)
+        } catch (error) {
+            res.status(400).json({
+                "message": error.message,
+                "error": true
+            })
+        }
+    }
 }
 
 module.exports = funcionario
